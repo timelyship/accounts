9ff43b81f4fca2256a925d2d37338f1e3131ae3f
Md Najim Ahmed <najim.ju@gmail.com>
2021-01-03 22:51:09 +0800

    refactor

--- controller/test_controller.go
+++ controller/test_controller.go
@@ -7,8 +7,8 @@ import (
 	"os"
 )
 
-func Decode(c *gin.Context)  {
-	tokenString:= c.GetHeader("Authorization")[len("Bearer "):]
+func Decode(c *gin.Context) {
+	tokenString := c.GetHeader("Authorization")[len("Bearer "):]
 	// Parse takes the token string and a function for looking up the key. The latter is especially
 	// useful if you use multiple keys for your application.  The standard is to use 'kid' in the
 	// head of the token to identify which key to use, but the parsed token (head and claims) is provided
@@ -16,7 +16,7 @@ func Decode(c *gin.Context)  {
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		// Don't forget to validate the alg is what you expect:
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
 
 		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
